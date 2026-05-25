629f2422c0076a8923e56f6ef5defa5f972f8fcd
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-25 09:23:20 +0530

    Backend - Attendance Login ViewSet - Login Function

--- backend/core/api/views.py
+++ backend/core/api/views.py
@@ -1,5 +1,9 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import EmployeeSerializer
+from rest_framework.decorators import action
+from .models import Employee, AttendanceLog
+from .serializers import EmployeeSerializer, AttendanceSerializer
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     """
@@ -8,3 +12,24 @@ class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     permission_classes = [IsAuthenticated]
+
+
+class AttendanceLogViewSet(viewsets.ModelViewSet):
+    """ 
+    Handle Add/Edit/Delete/List for all Attendance login data 
+    """
+    queryset = AttendanceLog.objects.all()
+    serializer_class = AttendanceSerializer
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['POST']) # Marks the Login of Employees
+    def mark_attendance(self, request):
+        Status = request.data.get('WorkStatus', 'In-Office')
+
+        NewLog = AttendanceLog.objects.create(
+            EmployeeRef = request.user,
+            WorkStatus = Status
+        )
+
+        SerializedData = self.get_serializer(NewLog)
+        return Response(SerializedData, status=201)
