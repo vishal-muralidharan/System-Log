7a8a7112c7357d674ddd6261518c712befd52b5e
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-26 15:37:51 +0530

    Frontend - Sidebar Fix - 1

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useNavigate, Link } from 'react-router-dom';
 import '../css/Sidebar.css';
 
 const Sidebar = () => {
     const NavigateTo = useNavigate()
+    const [Active, SetActive] = useState('Home')
 
     const HandleLogout =    () => {
             localStorage.removeItem('access_token')
@@ -15,15 +16,16 @@ const Sidebar = () => {
         <div className="sidebar-style">
             <ul className="sidebar-items">
                 <div className="main-items">
-                    <li className="list-item">
-                        <Link to="/dashboard" style={{ textDecoration: 'none', color: 'inherit' }}>Home</Link>
-                    </li>
-                    <li className="list-item">
-                        <Link to="/dashboard/history" style={{ textDecoration: 'none', color: 'inherit' }}>Attendance History</Link>
-                    </li>
-                    <li className="list-item">
-                        <Link to="/dashboard/profile" style={{ textDecoration: 'none', color: 'inherit' }}>Profile</Link>
-                    </li>
+                    <Link to="/dashboard" style={{ textDecoration: 'none', color: 'inherit' }} onClick={() => SetActive('Home')}>
+                        <li className={`list-item ${Active === 'Home' ? "active" : ""}`}>Home</li>
+                    </Link>
+                    <Link to="/dashboard/history" style={{ textDecoration: 'none', color: 'inherit' }} onClick={() => SetActive('History')}>
+                        <li className={`list-item ${Active === 'History' ? "active" : ""}`}>Attendance History</li>
+                    </Link>
+                    <Link to="/dashboard/profile" style={{ textDecoration: 'none', color: 'inherit' }} onClick={() => SetActive('Profile')}>
+                        <li className={`list-item ${Active === 'Profile' ? "active" : ""}`}>Profile</li>
+                    </Link>
+                    
                 </div>
                 <div>
                     <li className="logout-item" onClick={HandleLogout}>Logout</li>
