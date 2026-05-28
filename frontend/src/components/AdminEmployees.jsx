c0f5da80ab9df0d9a2f525d78f6856d24872a6e2
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-28 10:49:57 +0530

    Frontend - Admin - Attendance Logs - 2

--- frontend/src/components/AdminEmployees.jsx
+++ frontend/src/components/AdminEmployees.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import '../css/History.css';
+import axiosInstance from '../api/axios';
 
 const AdminEmployees = () => {
+  const [EmployeeData, SetEmployeeData] = useState(null)
+  const [Loading, SetLoading] = useState(true)
+  const [ErrorMsg, SetError] = useState('')
+
+  useEffect(() => {
+      const fetchEmployeeData = async () => {
+        try {
+          const Response = await axiosInstance.get('employees/')
+          const Logs = Response.data
+          const sortedLogs = [...Logs].sort((a, b) => a.id - b.id);
+
+          SetEmployeeData(sortedLogs)
+        }
+        catch (Error) {
+          console.error(Error)
+          SetError('Could not retrieve current data')
+        }
+        finally {
+          SetLoading(false)
+        }
+      }
+
+      fetchEmployeeData()
+    }, []
+  )
+  
+
+  const FormatTime = (isoString) => {
+    if (!isoString) return '--:--:--';
+      return new Date(isoString).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+  };
+
+  const FormatDate = (IsoString) => {
+        if (!IsoString) return '--';
+        return new Date(IsoString).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
+  };
+
+  if (Loading) {
+    return (
+      <h2 className='condition loading'>Loading your history...</h2>
+    )
+  }
+
   return (
-    <div>AdminEmployees</div>
+    <div className='outer'>
+      <h2>Employee Data</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Employee ID</th>
+            <th>Project</th>
+            <th>IsActive</th>
+          </tr>
+        </thead>
+        <tbody>
+          {EmployeeData.map((Data) => (
+            (!Data.IsAdmin) && (
+            <tr key={Data.id}>
+              <td>{Data.EmployeeId}</td>
+              <td>{Data.ProjectInvolved}</td>
+              <td>{Data.IsActive ? 'Yes' : 'No'}</td>
+            </tr>
+          )))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
