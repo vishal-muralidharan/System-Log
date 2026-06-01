0d7650623bac0c83d9f7683e9a2912aef3ebc5b6
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-01 16:16:09 +0530

    RN - Employee Template

--- frontend_rn/SystemLogApp/app/(employee)/index.tsx
+++ frontend_rn/SystemLogApp/app/(employee)/index.tsx
@@ -3,10 +3,18 @@ import React from 'react'
 
 export default function index() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>index</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white', 
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'black',
+        flex: 1
+    }
+})
