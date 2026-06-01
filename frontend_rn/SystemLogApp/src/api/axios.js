1ac0e8fccd8ab5d5f528e9eab72c4ed59aaaf97e
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-01 15:29:52 +0530

    RN - Django Connection

---
+++ frontend_rn/SystemLogApp/src/api/axios.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const API_BASE_URL = 'http://192.168.1.100:8000/api/'; 
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const accessToken = await AsyncStorage.getItem('access_token');
+    if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = await AsyncStorage.getItem('refresh_token');
+        const response = await axios.post(`${API_BASE_URL}auth/refresh/`, { refresh: refreshToken });
+        
+        const newAccess = response.data.access;
+        await AsyncStorage.setItem('access_token', newAccess);
+        originalRequest.headers.Authorization = `Bearer ${newAccess}`;
+        
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        await AsyncStorage.multiRemove(['access_token', 'refresh_token', 'user_profile']);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
