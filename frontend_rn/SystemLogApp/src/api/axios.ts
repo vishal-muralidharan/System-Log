d82bf7465e22beb900234fbb4b8dd9317b9b5227
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-03 09:01:56 +0530

    RN - Network IP Change

--- frontend_rn/SystemLogApp/src/api/axios.ts
+++ frontend_rn/SystemLogApp/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_BASE_URL = 'http://192.168.0.184:8000/api/'; 
+const API_BASE_URL = 'http://192.168.0.227:8000/api/'; 
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
